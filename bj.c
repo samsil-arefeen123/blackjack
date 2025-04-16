0709a11afd54ba0ab58c1a46bd7960a69f5c9874
Samsil Arefeen <2024-3-27-052@std.ewubd.edu>
2025-04-16 21:45:00 +0600

    Initial commit

---
+++ bj.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("Hello World\n");
+// This is a simple C program that prints "Hello World" to the console.
+ 
+}
+int blackjack(int a, int b){
+    
+    return a + b;
+}
